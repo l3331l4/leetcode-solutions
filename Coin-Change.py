760188374c3ae57b9998552ba4b1d043e36735da
Radi (Leila) Adil <adilr@tcd.ie>
2025-09-28 11:37:21 +0100

    [322] [Time Beats: 15.35%] [Memory Beats: 7.71%] - LeetPush

---
+++ Coin-Change.py
@@ -0,0 +1,25 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        # at each coin, either take it, or move to next coin
+        n = len(coins)
+        memo = {}
+
+        def howManyCoins(remaining):
+            if remaining == 0:
+                return 0
+            elif remaining < 0:
+                return float("inf")
+            elif remaining in memo:
+                return memo[remaining]
+            best = float("inf")
+
+            for coin in coins:
+                best = min(best, howManyCoins(remaining - coin) + 1)
+
+            memo[remaining] = best
+            return memo[remaining]
+        
+        res = howManyCoins(amount) 
+        if res == float("inf"):
+            return -1
+        return res
