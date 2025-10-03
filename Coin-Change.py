84b4925ce09e9256d4bc77798ac688ba076244fc
Radi (Leila) Adil <adilr@tcd.ie>
2025-10-03 14:18:20 +0100

    [322] [Time Beats: 63.38%] [Memory Beats: 32.12%] - LeetPush

--- Coin-Change.py
+++ Coin-Change.py
@@ -2,17 +2,27 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         # BOTTOM UP
         # DP[i] IS MIN NUMBER OF COINS TO GET i
+        if amount == 0:
+            return 0
 
+        n = len(coins)
         dp = [float("inf")] * (amount + 1)
         dp[0] = 0
 
-        for amt in range(1, amount+1):
+        for i in range(amount+1):
             for coin in coins:
-                if coin <= amt:
-                    dp[amt] = min(dp[amt], dp[amt - coin] + 1)
+                if i >= coin:
+                    dp[i] = min(1 + dp[i-coin], dp[i])
+
+        print(dp)
         
-        if dp[amount] == float("inf"):
+        res = dp[amount]
+        if res == float("inf"):
             return -1
         else:
-            return dp[amount]
+            return res
+
+
+
+
         
