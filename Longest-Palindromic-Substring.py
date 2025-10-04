77b904e1753173ac08f2b6c824345eb1facc96c2
Radi (Leila) Adil <adilr@tcd.ie>
2025-10-04 14:28:43 +0100

    [5] [Time Beats: 30.89%] [Memory Beats: 12.03%] - LeetPush

--- Longest-Palindromic-Substring.py
+++ Longest-Palindromic-Substring.py
@@ -1,23 +1,22 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         n = len(s)
-        dp = [[False] * n for _ in range(n)]
-        
+        dp = [ [False] * n for _ in range(n) ]
+        # DP[i][j] = is char i to char j a palindrome
+        start = 0
+        end = 0
+        maxLen = 0
+
         for i in range(n):
             dp[i][i] = True
 
-        maxLen = 0
-        start = 0
-        end = 0
-
         for i in range(n-1, -1, -1):
             for j in range(i+1, n):
-                if s[i] == s[j]:
-                    if j == i + 1 or dp[i+1][j-1]:
 
+                if s[i] == s[j]:
+                    if (j == i + 1) or (dp[i+1][j-1]):
                         dp[i][j] = True
                         length = j - i + 1
-
                         if length > maxLen:
                             maxLen = length
                             start = i
@@ -27,4 +26,3 @@ class Solution:
                         
 
         
-
