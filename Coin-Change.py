922d25afe217fefdc0587864be3ef476a3d0bf7f
Radi (Leila) Adil <adilr@tcd.ie>
2025-10-04 18:02:49 +0100

    [322] [Time Beats: 56.24%] [Memory Beats: 68.77%] - LeetPush

--- Coin-Change.py
+++ Coin-Change.py
@@ -1,28 +1,45 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        # BOTTOM UP
-        # DP[i] IS MIN NUMBER OF COINS TO GET i
-        if amount == 0:
-            return 0
+        # TOP DOWN
+        # dp = [None] * (amount + 1)
+        # dp[0] = 0
 
-        n = len(coins)
-        dp = [float("inf")] * (amount + 1)
+        # def minCoinsToAmount(amount):
+        #     if dp[amount] is not None:
+        #         return dp[amount]
+
+        #     minWays = float("inf")
+        #     for coin in coins:
+        #         if coin <= amount:
+        #             minWays = min(minWays, 1 + minCoinsToAmount(amount-coin) )
+            
+        #     dp[amount] = minWays
+            
+        #     return dp[amount]
+
+        # res = minCoinsToAmount(amount)
+        # if res == float("inf"):
+        #     return -1
+        # else:
+        #     return res
+
+        # BOTTOM UP
+
+        dp = [None] * (amount+1)
+        # dp[i] = min no. of coins to get amount i
         dp[0] = 0
 
-        for i in range(amount+1):
+        for amt in range(1, amount+1):
+            ways = float("inf")
             for coin in coins:
-                if i >= coin:
-                    dp[i] = min(1 + dp[i-coin], dp[i])
-
-        print(dp)
+                if coin <= amt:
+                    ways = min(ways, 1 + dp[amt - coin])
+            dp[amt] = ways
         
-        res = dp[amount]
-        if res == float("inf"):
+        if dp[amount] == float("inf"):
             return -1
         else:
-            return res
-
-
-
-
+            return dp[amount]
+                
+        
         
