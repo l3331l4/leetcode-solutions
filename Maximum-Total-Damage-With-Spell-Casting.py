25b191445d83a469aeac1b817aea69a4956372ac
Radi (Leila) Adil <adilr@tcd.ie>
2025-10-12 18:10:05 +0100

    [3186] [Time Beats: 48.34%] [Memory Beats: 97.16%] - LeetPush

---
+++ Maximum-Total-Damage-With-Spell-Casting.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maximumTotalDamage(self, power: List[int]) -> int:
+        earn = []
+        vals = []
+        
+        power.sort()
+        i = 0
+        while i < len(power):
+            v = power[i]
+            total = 0
+            while i < len(power) and power[i] == v:
+                total += v
+                i += 1
+            vals.append(v)
+            earn.append(total)
+
+        n = len(vals)
+        dp = [0] * (n+1)
+
+        for i in range(n-1, -1, -1):
+            next_idx = bisect_left(vals, vals[i] + 3)
+            take = earn[i] + dp[next_idx]
+            skip = dp[i+1]
+            dp[i] = max(take, skip)
+
+        return max(dp)
