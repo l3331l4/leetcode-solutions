fd2a2d5ecc67eb9ce18203055400777fb8c48db5
Radi (Leila) Adil <adilr@tcd.ie>
2025-10-14 23:04:39 +0100

    [125] [Time Beats: 82.18%] [Memory Beats: 68.19%] - LeetPush

--- Valid-Palindrome.py
+++ Valid-Palindrome.py
@@ -1,19 +1,13 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        
-        def isAlphaNum(c):
-            if (ord('a') <= ord(c) <= ord('z')) or (ord('A') <= ord(c) <= ord('Z') or c in "0123456789"):
-                return True
-            return False
-
         l = 0
         r = len(s) - 1
 
         while l < r:
-            while l < r and not (isAlphaNum(s[l])):
+            while l < r and not s[l].isalnum():
                 l += 1
 
-            while l < r and not (isAlphaNum(s[r])):
+            while l < r and not s[r].isalnum():
                 r -= 1
 
             if s[l].lower() != s[r].lower():
